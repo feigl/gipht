5f81ad45473fbc01ccc9a58ed5e2fef1b10c8e21
Kurt Feigl <feigl@wisc.edu>
2015-06-09 22:49:21 -0500

    Update main to include self documentation

---
+++ src/gipht_splash.m
@@ -0,0 +1,25 @@
+function gipht_splash
+% 
+% Copyright (c) Kurt Feigl, Cliff Thurber  All rights reserved. 
+% U.S. Patent No. 7,446,705.
+% 
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+% the Free Software Foundation, either version 3 of the License, or (at
+% your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+% 
+% You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+% along with this program. If not, see http://www.gnu.org/licenses/lgpl.html .
+% 
+% Source code: Copyright (c) Kurt Feigl, Cliff Thurber, Lee Powell, Peter
+% Sobol, Aaron Masters Elena Baluyut, S. Tabrez Ali 
+% University of Wisconsin-Madison'
+
+return
+
+
