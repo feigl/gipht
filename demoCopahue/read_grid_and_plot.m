590dea48c2d297e067fe8b19d1a5a0c932e0aeb2
Kurt Feigl <feigl@wisc.edu>
2015-07-21 17:33:23 -0500

    Move demoCopahue to GIPhTBox

--- demoCopahue/read_grid_and_plot.m
+++
@@ -1,8 +0,0 @@
-[Gx,Gy,Gv]= grdread2('Quad_ASC_CSK_2013.75_2014_04_09_lonlatvlos.grd');
-figure;
-imagesc(Gx,Gy,Gv*1000);axis xy
-cmapblacknan
-h=colorbar('Location','SouthOutside');
-xlabel(h,'mm/yr');
-title(h,'LOS velocity');
-printpdf('Quad_ASC_CSK_2013.75_2014_04_09_lonlatvlos.pdf');
