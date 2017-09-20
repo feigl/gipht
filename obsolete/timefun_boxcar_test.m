081f9a7503af4ca07de7568c7b7b67740823a9cf
Kurt Feigl <feigl@wisc.edu>
2017-09-19 22:10:50 -0500

    Add routines for riser time function.

---
+++ obsolete/timefun_boxcar_test.m
@@ -0,0 +1,14 @@
+clear all
+close all;
+
+% use datetime
+% tepochs = datenum(datetime(2017,1,1,12,0,0))+1:100;
+% treference1 = datenum(datetime(2017,5,1));
+% treference2 = datenum(datetime(2017,6,1));
+% use decimal years
+tepochs = dyear(2017,1,1)+[1:365]/365;
+treference1 = dyear(2017,5,1);
+treference2 = dyear(2017,6,1);
+ft=timefun_boxcar(tepochs,treference1,treference2);
+figure
+plot(tepochs,ft,'or');
