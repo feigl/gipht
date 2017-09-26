33493dba832deba419dc4e033d27a9449fe66dda
Kurt Feigl <feigl@wisc.edu>
2017-09-26 14:49:59 -0500

    Add routines for color tables.

--- utils/cmapgrayzero.m
+++ utils/cmapgrayzero.m
@@ -1,4 +1,4 @@
-function cmapgrayzero
+function cmap = cmapgrayzero
 % make a color map with zero set to gray
 cmap=colormap('jet');
 [nlevels,ndum] = size(cmap)
@@ -7,13 +7,16 @@ cmap=colormap('jet');
 % for i=1:nlevels-1
 %     cmap(i+1,:)=tmap(i,:);
 % end
-i1 = floor(nlevels/2)-1
-i2 = ceil(nlevels/2)+1
+i1 = floor(nlevels/2)-1;
+i2 = ceil(nlevels/2)+1;
 for i=i1:i2
     cmap(i,1:3) = [0.5 0.5 0.5]; % gray
 end
-for i=1:nlevels
-    fprintf(1,'%3d %10.4f %10.4f %10.4f\n',i,cmap(i,1),cmap(i,2),cmap(i,3));
-end
-colormap(cmap)
+% for i=1:nlevels
+%     fprintf(1,'%3d %10.4f %10.4f %10.4f\n',i,cmap(i,1),cmap(i,2),cmap(i,3));
+% end
+%colormap(cmap)
 return;
+end
+
+
